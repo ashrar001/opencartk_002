8e5e3948bf5e6ea33181f4d90fd28bad95d38de1
ashrarkhan <kknhak07@gmail.com>
2024-10-02 22:14:43 +0530

    implementation of log registration page

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -2,16 +2,24 @@ package testBase;
 
 import java.time.Duration;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-
+import org.testng.annotations.Parameters;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class BaseClass {
 	public WebDriver driver;
-
+    public Logger logger;
 	@BeforeClass
-	public void setup() {
+		public void setup() {
+		logger=LogManager.getLogger(this.getClass());
+				
 		driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
@@ -25,4 +33,18 @@ public class BaseClass {
 
 	}
 
+	public String randomString() {
+		String generatedString = RandomStringUtils.randomAlphabetic(5);
+		return generatedString;
+	}
+
+	public String randomNumeric() {
+		String randomnumber = RandomStringUtils.randomNumeric(10);
+		return randomnumber;
+	}
+
+	public String randomAlphaNumeric() {
+		String no = RandomStringUtils.randomAlphanumeric(10);
+		return no;
+	}
 }
--- src/test/java/testCase/TC001_Account_Regristration_Test.java
+++ src/test/java/testCase/TC001_Account_Regristration_Test.java
@@ -10,18 +10,26 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 import pageObject.Account_registration_page;
 import pageObject.HomePage;
 import testBase.BaseClass;
 
 public class TC001_Account_Regristration_Test extends BaseClass{
-	//WebDriver driver;
+	
 	@Test
     public void verify_Account_Registration() {
+		logger.info("**** Starting TC001_Account_Regristration_Test ***");
 		HomePage hp=new HomePage(driver);
+		try {
 		hp.clickMyAccount();
+		logger.info("**** click my account ***");
 		hp.clickRegister();
+		logger.info("**** click my register ***");
 		Account_registration_page arp=new Account_registration_page(driver);
+		logger.info("**** providing customer details**** ");
 		arp.setFirstName(randomString().toUpperCase());
 		arp.setLastName(randomString().toUpperCase());
 		arp.setEmail(randomString()+"@gmail.com");
@@ -31,20 +39,23 @@ public class TC001_Account_Regristration_Test extends BaseClass{
 		arp.setConfermPassword(alphaNum);
 		arp.setPrivacyPolicy();
 		arp.setButton();
+		logger.info("**** validating expected result**** ");
 		String actualmsg = arp.getConfirmation();
-		Assert.assertEquals(actualmsg, "Your Account Has Been Created!");
+		if(actualmsg.equals("Your Account Has Been Created!")) {
+			Assert.assertTrue(true);
+			
+		}
+		else {
+			logger.error("test failed_--__");
+			logger.debug("Debug logs**");
+			Assert.assertTrue(false);
+		}
+		}
+		catch(Exception e) {
+			Assert.fail();
+		}
+		logger.info("**** Finish TC001_Account_Regristration_Test**** ");
      }
-	public String randomString() {
-		String generatedString = RandomStringUtils.randomAlphabetic(5);
-				return generatedString;
-	}
-	public String randomNumeric() {
-		String randomnumber = RandomStringUtils.randomNumeric(10);
-		return randomnumber;
-	}
-	public String randomAlphaNumeric() {
-		String no=RandomStringUtils.randomAlphanumeric(10);
-		return no;
-	}
+	
 }
  
